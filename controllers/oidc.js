f9600bdf564d64ce6a63e676840eeb62e950885f
Marc Berchtold <me@echolot.io>
2018-07-26 22:16:29 +0200

    sample findById OIDC function

--- controllers/oidc.js
+++ controllers/oidc.js
@@ -5,17 +5,36 @@ class OIDCProviderController {
     constructor({app, router}) {
         this.app = app;
         this.router = router;
-        this.oidc = new OidcProvider(process.env.OIDC_ISSUER);
+
+        // Testurl: http://localhost:3000/oidc/auth?client_id=booki_webapp&redirect_uri=https://booki.me&response_type=id_token&scope=openid%20booki_user_api&nonce=123&state=321
+        this.oidc = new OidcProvider(process.env.OIDC_ISSUER, {
+            scopes: [
+                'booki_user_api'
+            ],
+            async findById(ctx, id) {
+                console.dir(ctx);
+                console.dir(id);
+                return {
+                    accountId: id,
+                    async claims(use, scope) { return { sub: id }; },
+                };
+            },
+        });
 
         oidc.loadKeystore().then(keystore => {
             return this.oidc.initialize({
-                keystore
+                keystore,
+                clients: [{
+                    client_id: process.env.OIDC_CLIENT_ID,
+                    grant_types: process.env.OIDC_GRANT_TYPES.split(','),
+                    response_types: process.env.OIDC_RESPONSE_TYPES.split(','),
+                    redirect_uris: process.env.OIDC_REDIRECT_URIS.split(','),
+                    token_endpoint_auth_method: 'none',
+                }],
             });
-        })
-        .then(() => {
-            this.router.use('/identity', this.oidc.callback);
-        })
-        .catch(err => {
+        }).then(() => {
+            this.router.use('/oidc', this.oidc.callback);
+        }).catch(err => {
             this.app.logger.error(err);
         });
     }
--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const { fsHelpers }     = require('./helpers');
 const App               = require('./app');
 
 if (!Fs.existsSync('.env')) {
-    console.log(`.env file not found, copying .env.example...`);
-    fsHelpers.copyFile('.env.example', '.env')
-    .then(() => {
+    console.log(`.env file not found, copying .env.example...`); // eslint-disable-line
+    fsHelpers.copyFile('.env.example', '.env').then(() => {
         require('dotenv-safe').config();
-        const app = new App();
+        new App();
     });
 } else {
     require('dotenv-safe').config();
-    const app = new App();
+    new App();
 }
